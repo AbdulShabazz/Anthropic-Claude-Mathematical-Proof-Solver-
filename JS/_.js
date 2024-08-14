b7bc3bd6b1b142ed9da1628d45533eba67d0afc6
Michelle Antonello <Seagat2011@users.noreply.github.com>
2024-08-13 21:40:24 -0400

    Update _.js (Major refactor: Add scope support)

--- JS/_.js
+++ JS/_.js
@@ -1,6 +1,8 @@
 
 try {
 
+    /** Benchmark: 9ms (test case 246) */
+    
     const NUM_WORKERS = 4;
     const _workerScript = 
 `
@@ -52,6 +54,36 @@ self.onmessage = function (e) {
     } // end applyRules
 }
 
+Object.prototype._scope_satisfied = function(etok,lhs,li,rhs,ri){
+    var i = 1;
+    var end_scope = { "(":")", "{":"}" };
+    var sat = true;
+    if (lhs[li] != rhs[ri]) {
+        sat = false;
+    } else if (etok in end_scope) {
+        if (((li+i) in lhs) && ((ri+i) in rhs)) {
+            var ltok = lhs [li+i];
+            var rtok = rhs [ri+i];
+            var I = rhs.length; // Math.min(lhs.length,rhs.length) //
+            etok = end_scope [etok];
+            while (i++<I){
+                if (ltok!=rtok){
+                    sat = false;
+                    break;
+                }
+                if(rtok == etok){
+                    break;
+                }
+                ltok = lhs[li+i];
+                rtok = rhs[ri+i];
+            }
+        } else {
+            sat = false;
+        }
+    } // test(etok) //
+    return sat;
+} // end Object.prototype._scope_satisfied
+
 Object.prototype._tryReplace = function (from, to) {
     let ret = false;
     if (from.length > this.length)
@@ -62,7 +94,8 @@ Object.prototype._tryReplace = function (from, to) {
     let tokenIDX = [];
     let rewriteFoundFlag;
     for (let tok of self) {
-        if (tok == from [i]){
+        if (self._scope_satisfied(tok,self,j,from,i) 
+                && from [i] === tok){
             tokenIDX.push (j);
             ++i;
         }
